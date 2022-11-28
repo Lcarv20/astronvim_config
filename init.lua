9c958b8d3d8d12dd10a8554c6fb9da1094866b55
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2022-11-28 02:29:09 +0100

    Adding packer as an ensured dep to be installed.

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ local config = {
     pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
     skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_reload = false, -- automatically reload and sync packer after a successful update
+    auto_reload = true, -- automatically reload and sync packer after a successful update
     auto_quit = false, -- automatically quit the current session after a successful update
     -- remotes = { -- easily add new remotes to track
     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
@@ -31,6 +31,7 @@ local config = {
   -- colorscheme = "dracula",
   -- colorscheme = "catppuccin",
   -- colorscheme = "ayu",
+  -- colorscheme = "gruvbox",
 
   -- Add highlight groups in any theme
   highlights = {
@@ -244,7 +245,7 @@ local config = {
       --     require("lsp_signature").setup()
       --   end,
       -- },
-
+      { "wbthomason/packer" },
       {
         "catppuccin/nvim",
         as = "catppuccin",
@@ -252,6 +253,7 @@ local config = {
       },
       { "Mofiqul/dracula.nvim" },
       { "ayu-theme/ayu-vim" },
+      { "morhetz/gruvbox" },
     },
     heirline = function(config)
       -- the first element of the default configuration table is the statusline
